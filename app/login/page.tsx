92594ec340ca5f739be7a49e93b00f19a7f441f5
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-01-22 19:34:21 -0300

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <main style={{ padding: 32, fontFamily: "system-ui" }}>
+      <h1>Login</h1>
+      <p>Em breve: autenticação.</p>
+      <a href="/">Voltar</a>
+    </main>
+  );
+}
