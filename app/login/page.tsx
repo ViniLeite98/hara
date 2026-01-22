6ce2e2fd5a5cc541cb5eae93b048d24560ec290f
ViniLeite98 <158287470+ViniLeite98@users.noreply.github.com>
2026-01-22 19:37:19 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,10 @@ export default function Login() {
     <main style={{ padding: 32, fontFamily: "system-ui" }}>
       <h1>Login</h1>
       <p>Em breve: autenticação.</p>
-      <a href="/">Voltar</a>
+      <a href="/" style={{ display: "inline-block", marginTop: 12 }}>
+  Voltar para a Home
+</a>
+
     </main>
   );
 }
